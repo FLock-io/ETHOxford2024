721028c090acf2f78120aeaab865cb5557cf46af
elihylui <elihylui@gmail.com>
2024-03-09 15:54:20 +0000

    init listenToEvents

--- packages/hardhat/scripts/listentoEvents.ts
+++ packages/hardhat/scripts/listentoEvents.ts
@@ -1,1 +1,21 @@
 import { ethers } from "hardhat";
+import ETHBridge from "../artifacts/contracts/ETHBridge.sol/ETHBridge.json";
+import FLRBridge from "../artifacts/contracts/FLRBridge.sol/FLRBridge.json";
+
+const ETH_BRIDGE_ADDRESS = "0xBA81FB1FddB2c281A617129842B7B9cA09217ae3";
+const FLR_BRIDGE_ADDRESS = "";
+
+async function main() {
+    console.log("Initiating...");
+
+    const ethprovider = new ethers.providers.JsonRpcProvider(
+        'https://sepolia-rpc.scroll.io/'
+      );
+
+    const deployer = new ethers.Wallet(
+        process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : "",
+        ethprovider
+    );
+
+
+}
