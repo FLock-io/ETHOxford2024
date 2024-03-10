cba652a35e83b33f11656e708ae3c06817412e7b
elihylui <elihylui@gmail.com>
2024-03-10 15:46:10 +0000

    test release WETH

--- packages/hardhat/scripts/nodeForEthBridge.ts
+++ packages/hardhat/scripts/nodeForEthBridge.ts
@@ -1,43 +1,64 @@
 import { ethers } from "hardhat";
 import FLRBridge from "../artifacts/contracts/FLRBridge.sol/FLRBridge.json";
+import ETHBridge from "../artifacts/contracts/ETHBridge.sol/ETHBridge.json";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const ETH_BRIDGE_CONTRACT_ADDRESS = "0xBA81FB1FddB2c281A617129842B7B9cA09217ae3"; 
+const FLR_BRIDGE_CONTRACT_ADDRESS = "0x07962C28579b1e76f3d39E860a077aA40aFe851C"; 
+const ETH_BRIDGE_CONTRACT_ADDRESS = "0xD92A717bf82CF506Af981230636e88dBdd2a1347";
+const providerApiKey = process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 async function main() {
     console.log("Starting the bridge listener...");
 
-    const ethprovider = new ethers.JsonRpcProvider(
-        'https://sepolia-rpc.scroll.io/'
+    const fLRprovider = new ethers.JsonRpcProvider(
+        'https://coston-api.flare.network/ext/C/rpc	'
       );
-    const deployer = new ethers.Wallet(
+
+    const fLR_deployer = new ethers.Wallet(
         process.env.DEPLOYER_PRIVATE_KEY ? process.env.DEPLOYER_PRIVATE_KEY : "",
-        ethprovider
+        fLRprovider
     );
 
-    const bridgeContract = new ethers.Contract(
-        ETH_BRIDGE_CONTRACT_ADDRESS,
+    const ETHProvider = new ethers.JsonRpcProvider(
+        `https://eth-sepolia.g.alchemy.com/v2/X1EpVDbiokKqFoMnR4_J9DToaCjjJy3E`
+    );
+
+    const ETHDeployer = new ethers.Wallet(
+        process.env.DEPLOYER_PRIVATE_KEY ? process.env.DEPLOYER_PRIVATE_KEY : "",
+        ETHProvider
+    );
+
+    const FLR_BridgeContract = new ethers.Contract(
+        FLR_BRIDGE_CONTRACT_ADDRESS,
         FLRBridge.abi,
-        deployer 
+        fLR_deployer 
     );
 
-    console.log("Listening for ReceiveWETH events...");
+    const ETH_BridgeContract = new ethers.Contract(
+        ETH_BRIDGE_CONTRACT_ADDRESS,
+        ETHBridge.abi,
+        ETHDeployer 
+    );
 
-    bridgeContract.on("ReceiveWETH", async (sender, value) => {
-        console.log(`ReceiveWETH event detected from ${sender} for ${(value)} WETH`);
+    console.log("Listening for ReceiveFLR events...");
+
+    ETH_BridgeContract.on("ReceiveETH", async (sender, value) => {
+        console.log(`ReceiveETH event detected from ${sender} for ${(value)} ETH`);
 
         // Call the releaseFLR function upon catching the event
         try {
-            console.log(`Attempting to release FLR to ${sender}...`);
-            const tx = await bridgeContract.releaseFLR(sender, value, {
+            console.log(`Attempting to release WETH to ${sender}...`);
+            const tx = await FLR_BridgeContract.releaseWETH(sender, value, {
                 gasLimit: 1000000, // Set an appropriate gas limit
             });
             const receipt = await tx.wait();
-            console.log(`FLR released successfully. Transaction Hash: ${receipt.transactionHash}`);
+            console.log(`WETH released successfully`);  
+            console.log(receipt.hash);
+
         } catch (error: any) {
-            console.error(`Failed to release FLR: ${error.message}`);
+            console.error(`Failed to release WETH: ${error.message}`);
         }
     });
 }
