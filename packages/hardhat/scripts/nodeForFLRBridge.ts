a9401836ea31116a38630be9ac6753e7f1153a6e
elihylui <elihylui@gmail.com>
2024-03-10 14:45:29 +0000

    fix nodeForFLRBridge.ts

--- packages/hardhat/scripts/nodeForFLRBridge.ts
+++ packages/hardhat/scripts/nodeForFLRBridge.ts
@@ -1,10 +1,12 @@
 import { ethers } from "hardhat";
 import FLRBridge from "../artifacts/contracts/FLRBridge.sol/FLRBridge.json";
+import ETHBridge from "../artifacts/contracts/ETHBridge.sol/ETHBridge.json";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const FLR_BRIDGE_CONTRACT_ADDRESS = "0xBA81FB1FddB2c281A617129842B7B9cA09217ae3"; 
+const FLR_BRIDGE_CONTRACT_ADDRESS = "0x07962C28579b1e76f3d39E860a077aA40aFe851C"; 
+const ETH_BRIDGE_CONTRACT_ADDRESS = "0xD92A717bf82CF506Af981230636e88dBdd2a1347";
 
 async function main() {
     console.log("Starting the bridge listener...");
@@ -17,27 +19,33 @@ async function main() {
         fLRprovider
     );
 
-    const bridgeContract = new ethers.Contract(
+    const FLR_BridgeContract = new ethers.Contract(
         FLR_BRIDGE_CONTRACT_ADDRESS,
         FLRBridge.abi,
         deployer 
     );
 
+    const ETH_BridgeContract = new ethers.Contract(
+        ETH_BRIDGE_CONTRACT_ADDRESS,
+        ETHBridge.abi,
+        deployer 
+    );
+
     console.log("Listening for ReceiveFLR events...");
 
-    bridgeContract.on("ReceiveFLR", async (sender, value) => {
+    FLR_BridgeContract.on("ReceiveFLR", async (sender, value) => {
         console.log(`ReceiveFLR event detected from ${sender} for ${(value)} FLR`);
 
         // Call the releaseFLR function upon catching the event
         try {
-            console.log(`Attempting to release WETH to ${sender}...`);
-            const tx = await bridgeContract.releaseWETH(sender, value, {
+            console.log(`Attempting to release WFLR to ${sender}...`);
+            const tx = await ETH_BridgeContract.releaseWFLR(sender, value, {
                 gasLimit: 1000000, // Set an appropriate gas limit
             });
             const receipt = await tx.wait();
-            console.log(`WETH released successfully. Transaction Hash: ${receipt.transactionHash}`);
+            console.log(`WFLR released successfully. Receipt Hash: ${receipt}`);
         } catch (error: any) {
-            console.error(`Failed to release WETH: ${error.message}`);
+            console.error(`Failed to release WFLR: ${error.message}`);
         }
     });
 }
